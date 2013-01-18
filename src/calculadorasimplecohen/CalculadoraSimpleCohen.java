36a1bb0a556d496726f10a250ac6ebbe71c9eb17
biblioteca <biblioteca@biblioteca2.upo.es>
2013-01-18 09:04:20 +0100

    Versión 0.4 Se han añadido comentarios sobre el tamaño del efecto y una pestaña.

---
+++ src/calculadorasimplecohen/CalculadoraSimpleCohen.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadorasimplecohen;
+
+import calculadoraGUI.GUI;
+
+/**
+ *
+ * @author p02sotos
+ * @version 0.1
+ * Calculadora simple de Cohen. 
+ * Este programa calcula la d de Cohen para dos muestras a partir
+ * de sus datos muestrales, media, desviación típica y tamaño de la muestra
+ * La fórmula de cálculo empleada se encuentra en.
+ * http://clbe.wordpress.com/2011/10/26/la-d-de-cohen-como-tamano-del-efecto/
+ * Visitar mi página de aprendizaje en java y estadística.
+ * http://estadisticaenjava.sergiosoriano.com/
+ * 
+ */
+public class CalculadoraSimpleCohen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUI ventana = new GUI();
+        
+        // TODO code application logic here
+    }
+}
